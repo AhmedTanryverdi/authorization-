9f0291302ffc62a83dcc00e3d5ff1e01a41728bd
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-09-24 18:10:59 +0300

    добавил форму в App

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
 import "./app.scss";
 import React from "react";
+import { Form } from "./components";
 
 export const App: React.FC = (): React.JSX.Element => {
-	return <div className="app">app</div>;
+	return (
+		<div className="app">
+			<Form />
+		</div>
+	);
 };
